163c530c1412731e452b098047569e2db1bae52d
Ajyke <64535386+Ajyke@users.noreply.github.com>
2023-04-10 21:00:29 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.196.188.92:9000/',
+  serverUrl: 'http://54.91.157.66:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
